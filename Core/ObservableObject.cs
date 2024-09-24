129565bd85ec1bb16f7627f1eb848eba5f549c07
paulkaranu001 <paulkaranu@live.com>
2024-09-25 01:19:24 +0300

    Add project files.

---
+++ Core/ObservableObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModernDesign.Core
+{
+    internal class ObservableObject : InotifyPropertyChnaged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
