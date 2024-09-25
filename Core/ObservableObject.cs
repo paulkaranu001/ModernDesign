9f9fc27b415c76e3d01527a5a14709dfe82d77f6
paulkaranu001 <paulkaranu@live.com>
2024-09-26 02:06:44 +0300

    New class and styles created

--- Core/ObservableObject.cs
+++ Core/ObservableObject.cs
@@ -2,13 +2,19 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModernDesign.Core
 {
-    internal class ObservableObject : InotifyPropertyChnaged
+    internal class ObservableObject : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
---
+++ Core/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ModernDesign.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canexecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canexecute = null)
+        {
+            _execute = execute;
+            _canexecute = canexecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canexecute == null || _canexecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
